6292867d1b36d98cc41047763c947bb79803724c
B. Haki Martin <jojackblack@gmail.com>
2024-08-19 19:49:08 -0400

    question form functionality works, just need to connect to mongo

--- src/lib/components/CreateQuestion.tsx
+++ src/lib/components/CreateQuestion.tsx
@@ -2,6 +2,7 @@
 
 import React, { ChangeEvent, ChangeEventHandler, FormEvent, useState } from 'react'
 import { difficultyLevel } from '../model/iquestion';
+import { clear } from 'console';
 
 const CreateQuestion = () => {
     const [showForm, setShowForm] = useState<boolean>(false);
@@ -28,18 +29,68 @@ const CreateQuestion = () => {
             subject: string,
             notes: string
         } = {...form};
+        
         updatedForm[key as keyof typeof updatedForm] = value;
+        
+        const newForm:{
+            question: string,
+            docs: string,
+            subject: string,
+            notes: string,
+            level: difficultyLevel
+        } = {...updatedForm, level:form.level};
+        setForm(newForm);
     }
 
     function handleDifficultyChange(event:{target:{value:string}}):void{
-        switch (event.target.value){}
+        console.log(event.target.value);
+
+        const updatedForm:{
+            question: string,
+            docs: string,
+            subject: string,
+            notes: string,
+            level: difficultyLevel
+        }={...form};
+
+        switch (event.target.value){
+            case "0":
+                updatedForm.level = difficultyLevel.beginner;
+                console.log("beginner")
+                break;
+            case "1":
+                updatedForm.level = difficultyLevel.intermediate;
+                console.log("intermediate")
+                break;
+            case "2":
+                updatedForm.level = difficultyLevel.advance;
+                console.log("advance")
+                break;
+        }
+
+        setForm(updatedForm);
+    }
+
+    function handleSubmit(e:FormEvent){
+        e.preventDefault();
+        clearForm();
+    }
+
+    function clearForm(){
+        setForm({
+            question: "",
+            docs: "",
+            subject: "JavaScript",
+            notes: "",
+            level: difficultyLevel.beginner
+        });
     }
 
   return (
     <>
         <button onClick={()=>setShowForm(!showForm)}>{showForm? "Hide Question Form":"Show Question Form"}</button>
         {showForm? 
-            <form className='bg-gray-900 border space-y-2'>
+            <form className='bg-gray-900 border space-y-2' onSubmit={handleSubmit}>
                 Question: <input className='text-black font-semibold' name='question' value={form.question} onChange={handleFormChange} />
                 <br />
                 Official Docs: <input className='text-black font-semibold' name='docs' value={form.docs} onChange={handleFormChange} />
@@ -47,10 +98,17 @@ const CreateQuestion = () => {
                 Notes: <input className='text-black font-semibold' name='notes' value={form.notes} onChange={handleFormChange} />
                 <br />
                 Subject:
-                <select className='text-black font-semibold' name='subject' value={form.subject} onChange={handleFormChange} >
+                <select className='text-black font-semibold px-2 py-1' name='subject' value={form.subject} onChange={handleFormChange} >
                     <option value={"JavaScript"}>JavaScript</option>
                     <option value={"React"}>React</option>
                     <option value={"Angular"}>Angular</option>
+                </select>
+                <br />
+                Difficulty:
+                <select className='text-black font-semibold px-2 py-1' name='difficulty' value={form.level} onChange={handleDifficultyChange} >
+                    <option value={"0"}>Beginner</option>
+                    <option value={"1"}>Intermediate</option>
+                    <option value={"2"}>Advance</option>
                 </select>
                 <br />
                 <button className='bg-gray-200 text-black font-semibold border-8 border-black rounded-full py-1 px-2 mx-8' type='submit'>Submit</button>
