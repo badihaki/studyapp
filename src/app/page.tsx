3b135f5b2c44133c6244fafbb803db44c1f5b5fa
B. Haki Martin <jojackblack@gmail.com>
2024-08-30 12:14:45 -0400

    added search bar that filters questions on the main page based on user input. searches through everything - question, docs, tags and notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,27 @@
 
 import CardHolder from "@/lib/components/CardHolder";
 import Header from "@/lib/components/Header";
+import SearchBar from "@/lib/components/SearchBar";
 import { useAppSelector } from "@/lib/redux/hooks";
+import { useState } from "react";
 
 export default function Home() {
   const questions = useAppSelector(state => state.questionSlice);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const filteredQuestions = questions.filter((q)=>{
+    if(q.question.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()) || q.tags.map(tag=>tag.toLowerCase()).includes(searchQuery.toLowerCase()) || q.docs.toLowerCase().includes(searchQuery.toLowerCase()) || q.notes.includes(searchQuery)){
+      return q
+    }
+  });
 
   return (
     <main className="justify-center w-full h-full">
       <Header text="Study App Home" />
+      <div>
+        <SearchBar searchQueryValue={searchQuery} changeSearchQuery={setSearchQuery} />
+      </div>
       <div className="w-fit mx-auto text-center justify-center">
-        <CardHolder questions={questions} />
+        <CardHolder questions={filteredQuestions} />
       </div>
     </main>
   );
--- src/lib/components/SearchBar.tsx
+++ src/lib/components/SearchBar.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const SearchBar = () => {
+const SearchBar = (props:{searchQueryValue:string, changeSearchQuery:(value:string)=>void}) => {
+  const handleInputChange = (e:{target:{value:string}}) => {
+    // console.log(e.target.value);
+    props.changeSearchQuery(e.target.value);
+  }
+
   return (
-    <div>
-      search bar
+    <div 
+    className='text-center'>
+      <div className='font-semibold font-serif'>
+        Filter Questions
+      </div>
+      <div >
+        <input value={props.searchQueryValue} onChange={handleInputChange}
+        className='border-2 border-stone-500 font-semibold text-sm h-fit w-fit text-black text-center' />
+      </div>
     </div>
   )
 }
